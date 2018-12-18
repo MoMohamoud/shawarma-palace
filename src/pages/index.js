6c3c074abbbadcaeeb96ff440c4e03da57be35a6
Mahamoud <mmohamoud@dosarrest.com>
2018-12-18 01:27:25 -0800

    updata

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,107 +1,65 @@
-import React from 'react';
+import React, {Fragment} from 'react';
 import {Link, graphql} from 'gatsby';
+import delay from 'delay';
 
 import Bio from '../components/bio';
 import Layout from '../components/layout';
 import SEO from '../components/seo';
 import {rhythm} from '../utils/typography';
 import {Parallax, ParallaxLayer} from 'react-spring/addons';
+import {Keyframes, animated} from 'react-spring';
+
 import './styles.css';
 
-// const styles = {
-//   .container > div > div {
-//     display: flex;
-//     align-items: center;
-//     justify-content: center;
-//   }
+let selectedIndexValue = 0;
 
-//   .text {
-//     pointer-events: none;
-//     justify-content: start !important;
-//     font-family: 'Kanit', sans-serif;
-//     line-height: 0px;
-//     text-transform: uppercase;
-//   }
+const myTimer = value => {
+  console.log ('value', value);
+  selectedIndexValue += 1;
+};
+console.log ('here');
+// const myVar = setInterval(myTimer, 1000);
+const setTimeoutAsync = function (value) {
+  new Promise (r => setInterval (myTimer, 10000, value));
+};
 
-//   .number {
-//     font-size: 450px;
-//     color: #373c4c;
-//   }
+const Page = ({offset, caption, first, second, gradient, onClick}) => {
+  return (
+    <React.Fragment>
+      <ParallaxLayer offset={offset} speed={0.2} onClick={onClick}>
+        <div className="slopeBegin" />
+      </ParallaxLayer>
 
-//   .number span {
-//     display: inline-block;
-//     position: relative;
-//     transform: translate3d(-35%, 0, 0);
-//   }
+      <ParallaxLayer offset={offset} speed={-0.2} onClick={onClick}>
+        <div className={`slopeEnd ${gradient}`} />
+      </ParallaxLayer>
 
-//   .header {
-//     margin-left: 350px;
-//     font-size: 70px;
-//     color: white;
-//   }
+      {/* <ParallaxLayer className="text number" offset={offset} speed={0.3}>
+        <span>{second}</span>
+      </ParallaxLayer> */}
 
-//   .stripe {
-//     height: 2px;
-//     width: auto;
-//   }
-
-//   .slopeBegin {
-//     background-color: #20232f;
-//     clip-path: polygon(20vw 0, 70% 0, calc(70% - 20vw) 100%, 0 100%);
-//   }
-
-//   .slopeEnd {
-//     clip-path: polygon(70% 0, 100% 0, calc(100% - 20vw) 100%, calc(70% - 20vw) 100%);
-//   }
-
-//   .slopeBegin,
-//   .slopeEnd {
-//     position: absolute;
-//     width: 140%;
-//     height: 100%;
-//     cursor: pointer;
-//   }
-
-//   // .teal {
-//   //   background: linear-gradient(to right, SlateBlue 0%, DeepSkyBlue 100%);
-//   // }
-
-//   // .tomato {
-//   //   background: linear-gradient(to right, tomato 0%, gold 100%);
-//   // }
-// };
-
-const Page = ({offset, caption, first, second, gradient, onClick}) => (
-  <React.Fragment>
-    <ParallaxLayer offset={offset} speed={0.2} onClick={onClick}>
-      <div className="slopeBegin" />
-    </ParallaxLayer>
-
-    <ParallaxLayer offset={offset} speed={-0.2} onClick={onClick}>
-      <div className={`slopeEnd ${gradient}`} />
-    </ParallaxLayer>
-
-    <ParallaxLayer className="text number" offset={offset} speed={0.3}>
-      <span>0{offset + 1}</span>
-    </ParallaxLayer>
-
-    <ParallaxLayer className="text header" offset={offset} speed={0.4}>
-      <span>
-        Catering Options
-        <p style={{fontSize: 20}}>{caption}</p>
-        <div className={`stripe ${gradient}`} />
-        <p>{first}</p>
-        <p>{second}</p>
-      </span>
-    </ParallaxLayer>
-  </React.Fragment>
-);
+      <ParallaxLayer className="text header" offset={offset} speed={0.4}>
+        <span>
+          {/* <p style={{fontSize: 20}}>{caption}</p> */}
+          {/* <div className={`stripe ${gradient}`} /> */}
+          {/* <p>{first}</p> */}
+          {/* <p>{second}</p> */}
+        </span>
+      </ParallaxLayer>
+    </React.Fragment>
+  );
+};
 
 class BlogIndex extends React.Component {
   render () {
     const {data} = this.props;
     const siteTitle = data.site.siteMetadata.title;
     const posts = data.allMarkdownRemark.edges;
+    console.log('offset', this.refs.parallax)
+
+    // if (this && this.refs.parallax && this.refs.parallax) {
+    //   setTimeoutAsync (this.refs.parallax);
+    // }
 
     return (
       <div>
@@ -110,28 +68,28 @@ class BlogIndex extends React.Component {
           ref="parallax"
           pages={3}
           horizontal
-          scrolling={true}
+          scrolling
         >
           <Page
             offset={0}
             gradient="pink"
             caption="Gold Menu"
             first="Feeds 12-16 People"
-            second="Display Images"
+            second="Gold Menu"
           />
           <Page
             offset={1}
             gradient="teal"
             caption="Silver Catering Option"
             first="Feeds 8-12"
-            second="Display Images"
+            second="Silver Menu"
           />
           <Page
             offset={2}
             gradient="tomato"
             caption="Bronze Catering Option"
             first="Feeds 4-8"
-            second="Display Images"
+            second="Bronze Menu"
           />
         </Parallax>
       </div>
